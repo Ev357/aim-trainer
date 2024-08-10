fd084824d08ffcdbd624695d3c7659e6d4fd4731
Ev357 <evest.dev@gmail.com>
2024-08-10 15:39:13 +0200

    build: some basic setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,14 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
+
   future: {
     compatibilityVersion: 4,
+  },
+
+  modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt"],
+  shadcn: {
+    prefix: "U",
+    componentDir: "./app/components/ui",
   },
 });
